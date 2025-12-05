f6541205472f9e09cd9ca2caad23919cf177c9a9
MaximSokolov18 <makssokolov107@gmail.com>
2025-12-06 00:31:36 +0100

    Day01 - Part 2 - Done

--- src/day01/day01.ts
+++ src/day01/day01.ts
@@ -1,4 +1,4 @@
-import {rotations} from './constants'; 
+import {rotations} from './constants';
 
 let placeOfPoint = 50;
 let zeroCount = 0;
@@ -8,20 +8,31 @@ function solveDay01() {
     
     arrayOfRotations.forEach((rotation) => {
         const side = rotation[0];
-        const movePoints = Number(rotation.slice(1).slice(-2));
+        const fullMovePoints = rotation.slice(1);
+        const movePoints = Number(fullMovePoints.slice(-2));
+
+        zeroCount += Math.floor(Number(fullMovePoints) / 100);
 
         const newPlaceOfPoint = placeOfPoint + (side === 'R' ? movePoints : -movePoints);
 
         if (newPlaceOfPoint > 99) {
+            if (placeOfPoint !== 0) {
+                zeroCount++;
+            }
+
             placeOfPoint = newPlaceOfPoint % 100;
         } else if (newPlaceOfPoint < 0) {
+            if (placeOfPoint !== 0) {
+                zeroCount++;
+            }
+
             placeOfPoint = 100 + newPlaceOfPoint;
         } else {
             placeOfPoint = newPlaceOfPoint;
-        }
 
-        if (placeOfPoint === 0) {
-            zeroCount++;
+            if (placeOfPoint === 0) {
+                zeroCount++;
+            }
         }
     })
 
