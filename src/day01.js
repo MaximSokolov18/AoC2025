07598f5689a7d674682ce6fa9b5f3e701131dd36
m.sokolov <maksym.sokolov@pitchbook.com>
2025-12-05 23:41:25 +0100

    Initialize TypeScript setup and restructure project files

--- src/day01.js
+++
@@ -1,9 +0,0 @@
-// Day 1 Solution
-console.log("Running Day 1 solution...");
-
-function solveDay01() {
-  // Your solution code here
-  console.log("Day 1: Solution not implemented yet");
-}
-
-solveDay01();
